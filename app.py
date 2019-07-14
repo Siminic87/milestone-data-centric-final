7b3656c4df2dc4f52e04c0b78c22bf5bc1cc75eb
Ubuntu <ubuntu@ip-172-31-86-34.ec2.internal>
2019-07-14 20:34:36 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
